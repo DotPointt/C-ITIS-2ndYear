1212c5a761764092064e20d8d68c0422cf2b19c7
DotPointt <daukaevk@gmail.com>
2023-11-01 19:00:40 +0300

    added task and 1st method duble2

---
+++ StringLession/MyTemplate/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace MyTemplate
+{
+    public static class Mapper
+    {
+        public static string Method1(string name)
+        {
+            return "Здравсвтуйте @{name}, вы отчислены".Replace("@{name}", name);
+        }
+
+        public static string Method2(object obj)
+        {
+            var name = obj.GetType().GetProperty("name").GetValue(obj);
+            return "Здравствуйте, @{name}. Вы прописаны по адресу @{address}.".Replace("@{name}", (string)name);
+        }
+
+        
+    }
+}
